8ec9cadc29ab02b3fd697c5b4c70adc252fee966
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-24 04:43:01 +0600

    Add Icons and handle Show All jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,8 @@ import Job from '../Job/Job';
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  // is not the best way to show all data
+  const [dataLength, setDataLength] = useState(4);
   useEffect(() => {
     fetch('./jobs.json')
       .then((res) => res.json())
@@ -18,9 +20,19 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div
+        className={dataLength === jobs.length ? 'hidden' : 'w-[20%] mx-auto'}
+      >
+        <button
+          onClick={() => setDataLength(jobs.length)}
+          className="btn my-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500"
+        >
+          Show All
+        </button>
       </div>
     </div>
   );
